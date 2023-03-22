fbc6e80c40b99ac104f1b0b68d16af1aa176918e
uhammgm <uhammgm@stu.ccsdut.org>
2023-03-22 11:30:38 -0600

    lots of work done

--- Assignment5PTII.py
+++ Assignment5PTII.py
@@ -1,6 +1,7 @@
 import math
 #name1 = input("What is the name of person 1? ")
 #name2 = input("What is the name of person 2? ")
+print("     Welcome to")
 posx1 = int(input("What is the X position of person 1? "))
 posy1 = int(input("What is the Y position of person 1? "))
 posx2 = int(input("What is the X position of person 2? "))
@@ -14,13 +15,20 @@ pos2 = str(posx2) + " " + str(posy2)
 spacestat = 0
 
 distance = math.sqrt(((posx2 - posx1)**2) + ((posy2 - posy1)**2) )
+if distance < space1 + space2:
+    print("Spaces overlap")
 if distance > space1 + space2:
-    spacestat = 1
-else:
-    spacestat = 2
-
-if spacestat == 1:
-    print("There are no conflicts.")
-if spacestat == 2:
-    print("Conflict detected.")
+    print("No overlap")
+#inside space
+if space1 > distance + space2:
+    print("Person2 space inside person1 space")
+if space2 > distance + space1:
+    print("Person1 space inside person2 space")
+#person inside space
+if distance < space2 and distance < space1:
+    print("both in space")
+elif distance < space1:
+    print("person1 is in person2 space")
+elif distance < space2:
+    print("Person2 is in person1 space")
 
--- Practice again.py
+++ Practice again.py
@@ -1,9 +1,22 @@
-count = 0
-list = ["Hey","this",15,"cool."]
+def smpf(n):
+    """
+    Returns the smallest prime factor of n.
+    """
+    if n % 2 == 0:
+        return 2
+    for i in range(3, int(n ** 0.5) + 1, 2):
+        if n % i == 0:
+            return i
+    return n
 
-for i in list:
-    print(i)
-    count += 1
+def S(n):
+    """
+    Returns the sum of smpf(i) for 2 <= i <= n.
+    """
+    result = 0
+    for i in range(2, n + 1):
+        result += smpf(i)
+    return result
 
-print(" ")
-print("My for loop ran " + str(count) + " times.")
+# Example usage:
+print(S(5000))  # Output: 1257
